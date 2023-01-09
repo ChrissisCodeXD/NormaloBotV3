c7b4eec3eb5dead4f839be13f97dd482cab4469d
Chrissi <schwarzlichtampel@gmail.com>
2023-01-09 19:16:06 +0100

    init

--- src/helper/__init__.py
+++ src/helper/__init__.py
@@ -1,4 +1,8 @@
 from .env import Env
 from .HelpCommand import HelpCommand
 from .datastructs import UserEntry, GuildEntry
-from .permcheck import mod_check
+from .permcheck import perm_check
+from .LRU import PermissionCache, LruCache
+from .get_time import get_time
+from .color import Color, Colour
+from .similar import similar
