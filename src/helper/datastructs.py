03b260611e6c9cc0ba9237150c90676e1d836b3f
Chrissi <schwarzlichtampel@gmail.com>
2023-01-09 06:01:10 +0100

    datastructs.py: added my own implementation of datastructs and called them hashable because I can hash them? idk why i did this took me way to long

---
+++ src/helper/datastructs.py
@@ -0,0 +1,35 @@
+from src.imports import *
+
+
+class Hashable:
+    def __eq__(self, other):
+        if other.__class__ is not self.__class__:
+            return NotImplemented
+        return self.__dict__ == other.__dict__
+
+    def __hash__(self):
+        return hash(tuple(sorted(self.__dict__.items())))
+
+    def __repr__(self):
+        return str(
+            self.__class__.__name__ + "(" + ", ".join(
+                [f"{k}={v!r}" for k, v in self.__dict__.items()]
+            ) + ")"
+        )
+
+
+
+class UserEntry(Hashable):
+    user_id: int
+    user_name: str
+    user_discriminator: str
+    user_avatar: str
+    user_bot: bool
+
+
+class GuildEntry(Hashable):
+    guild_id: int
+    guild_name: str
+    guild_icon: str
+    guild_owner_id: int
+    guild_member_count: int
