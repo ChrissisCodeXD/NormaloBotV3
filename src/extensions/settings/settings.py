0e9fa3eaa5ba6c4ecae8a1bd333460317ebadc52
Ray <88249929+ChrissisCodeXD@users.noreply.github.com>
2023-01-10 09:13:54 +0100

    settings.py: Added autocomplete for cmd setting

--- src/extensions/settings/settings.py
+++ src/extensions/settings/settings.py
@@ -2,6 +2,7 @@ import hikari
 
 from src.imports import *
 import src
+from typing import Union, Sequence
 
 settings_plugin = lightbulb.Plugin("settings.settings")
 settings_plugin.add_checks(
@@ -9,8 +10,12 @@ settings_plugin.add_checks(
     lightbulb.checks.bot_only
 )
 
+
+
+
 @settings_plugin.command()
 @lightbulb.check_exempt(src.helper.perm_check)
+@lightbulb.option("command","The Command where you wanna change the permissions",type=str,autocomplete=True)
 @lightbulb.command("permission_settings", "settings for who can use which commands")
 @lightbulb.implements(lightbulb.PrefixCommand, lightbulb.SlashCommand)
 async def permissionSettings(ctx: lightbulb.Context) -> None:
@@ -21,6 +26,15 @@ async def permissionSettings(ctx: lightbulb.Context) -> None:
         timestamp=src.helper.get_time()
     )
 
+@command.autocomplete("command")
+async def command_autocomplete(
+        opt: hikari.AutocompleteInteractionOption, inter: hikari.AutocompleteInteraction
+) -> Union[str, Sequence[str], hikari.CommandChoice, Sequence[hikari.CommandChoice]]:
+    toret = []
+    for command_name in settings_plugin.app.slash_commands:
+        if command_name.startswith(opt.value):
+            toret.append(command_name)
+    return toret
 
 
 
