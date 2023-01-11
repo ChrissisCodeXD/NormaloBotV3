a0666c3304111d4285d258fd54e6bc4a4cd32f52
Chrissi <schwarzlichtampel@gmail.com>
2023-01-11 21:44:42 +0100

    init update

--- src/helper/__init__.py
+++ src/helper/__init__.py
@@ -1,9 +1,10 @@
 from .env import Env
 from .HelpCommand import HelpCommand
 from .datastructs import UserEntry, GuildEntry
-from .permcheck import perm_check
+from .permcheck import perm_check, perm_check_without_ctx
 from .LRU import PermissionCache, LruCache
 from .get_time import get_time
 from .color import Color, Colour
 from .similar import similar
 from .decorators import parse_options
+from .generate_id import generate_id
